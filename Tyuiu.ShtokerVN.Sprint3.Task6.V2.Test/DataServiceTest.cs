cf4cfc0519ae6d5999addc8e07cf449a89021475
Vladimir Shtoker <Tyuiu.ShtokerVN@outlook.com>
2023-10-23 20:03:59 +0500

    Task6

---
+++ Tyuiu.ShtokerVN.Sprint3.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShtokerVN.Sprint3.Task6.V2.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint3.Task6.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 12;
+            int stopValue = 18;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 120;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShtokerVN.Sprint3.Task6.V2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShtokerVN.Sprint3.Task6.V2.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint3.Task6.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры C#                                                                                *");
+            Console.WriteLine("* Задание #6                                                                                                              *");
+            Console.WriteLine("* Вариант #2                                                                                                              *");
+            Console.WriteLine("* Выполнил: Штокер Владимир Николаевич | ИИПБ-23-1                                                                        *");
+            Console.WriteLine("***************************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                                *");
+            Console.WriteLine("* Написать программу, которая ищет среди целых чисел, принадлежащих числовому отрезку [12, 18] количество всех делителей. *");
+            Console.WriteLine("*                                                                                                                         *");
+            Console.WriteLine("***************************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                        *");
+            Console.WriteLine("***************************************************************************************************************************");
+
+            int startValue = 12;
+            int stopVaue = 18;
+
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopVaue);
+
+            Console.WriteLine("***************************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                              *");
+            Console.WriteLine("***************************************************************************************************************************");
+
+            Console.WriteLine("Количество делителей = " + ds.GetSumTheDivisors(startValue, stopVaue));
+
+            Console.ReadKey();
+        }
+    }
+}
