bce7b0e67f7746beee518d6c7517334d1b0b5654
Vladimir Shtoker <Tyuiu.ShtokerVN@outlook.com>
2023-10-24 02:18:16 +0500

    Task7

---
+++ Tyuiu.ShtokerVN.Sprint3.Task7.V7.Test/DataServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShtokerVN.Sprint3.Task7.V7.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint3.Task7.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = 17.23;
+            valueWaitArray[1] = 13.51;
+            valueWaitArray[2] = 10.34;
+            valueWaitArray[3] = 7.79;
+            valueWaitArray[4] = 5.00;
+            valueWaitArray[5] = 0.00;
+            valueWaitArray[6] = 0.08;
+            valueWaitArray[7] = -4.62;
+            valueWaitArray[8] = -8.32;
+            valueWaitArray[9] = -10.82;
+            valueWaitArray[10] = -12.66;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.ShtokerVN.Sprint3.Task7.V7/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShtokerVN.Sprint3.Task7.V7.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint3.Task7.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры C#                                                                                *");
+            Console.WriteLine("* Задание #7                                                                                                              *");
+            Console.WriteLine("* Вариант #7                                                                                                              *");
+            Console.WriteLine("* Выполнил: Штокер Владимир Николаевич | ИИПБ-23-1                                                                        *");
+            Console.WriteLine("***************************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции: F(x) = cos(x) + cos(x) / x + 2 - 3 * x                    *");
+            Console.WriteLine("* (произвести табулирование) f(x) на заданном диапазоне [-5;5] с шагом 1. Произвести проверку деления на ноль.            *");
+            Console.WriteLine("* При делении на ноль вернуть 0. Значения округлить до двух знаков после запятой.                                         *");
+            Console.WriteLine("*                                                                                                                         *");
+            Console.WriteLine("***************************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                        *");
+            Console.WriteLine("***************************************************************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                              *");
+            Console.WriteLine("***************************************************************************************************************************");
+
+
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|     X    +   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <=len-1; i++ )
+            {
+                Console.WriteLine("| {0,5:d}    |  {1, 6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+        }
+    }
+}
