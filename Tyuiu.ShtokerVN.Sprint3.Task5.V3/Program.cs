c485c2d2307f309b14b55a487f0e4f556edb0868
Vladimir Shtoker <Tyuiu.ShtokerVN@outlook.com>
2023-10-23 19:18:27 +0500

    Task5

---
+++ Tyuiu.ShtokerVN.Sprint3.Task5.V3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShtokerVN.Sprint3.Task5.V3.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint3.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                       *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры C#                                                        *");
+            Console.WriteLine("* Задание #5                                                                                      *");
+            Console.WriteLine("* Вариант #3                                                                                      *");
+            Console.WriteLine("* Выполнил: Штокер Владимир Николаевич | ИИПБ-23-1                                                *");
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                        *");
+            Console.WriteLine("* Написать программу, которая вычисляет сумму сумм ряда, при x = 5                                *");
+            Console.WriteLine("*                                                                                                 *");
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                *");
+            Console.WriteLine("***************************************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 11;
+
+            Console.WriteLine("Переменная X = " + x);
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага воторой суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+
+            Console.WriteLine("***************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                      *");
+            Console.WriteLine("***************************************************************************************************");
+
+            Console.WriteLine("Сумма сумм ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+            Console.ReadKey();
+
+        }
+    }
+}
